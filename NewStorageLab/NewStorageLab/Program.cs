dcfaae9753bb9c0edca295cc357010498552b5ce
krindioom <47291437+krindioom@users.noreply.github.com>
2024-09-12 21:05:17 +0700

    Настроил контекст

--- NewStorageLab/NewStorageLab/Program.cs
+++ NewStorageLab/NewStorageLab/Program.cs
@@ -1,3 +1,7 @@
+using DAL = NewStorageLab.DAL;
+using System;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<DAL.AppDbContext>(opt =>
+{
+    opt.UseSqlite(connectionString);
+});
 
 var app = builder.Build();
 
